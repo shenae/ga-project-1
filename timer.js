44a7000a157650e3b1a8b2a5cff7d422ff63ced9
Shenae Simmons <shhnae@gmail.com>
2019-04-04 21:49:53 -0400

    project complete

--- timer.js
+++ timer.js
@@ -1,17 +1,18 @@
-//declare time
+
 let counter = setInterval(countdown, 1000);
 let startTime = 11;
 
-//timer function
+
 function countdown() {
 	if(startTime === 0) {
 		clearInterval(counter);
 		document.getElementById('gifs').innerHTML = 'PRESS THE NEXT BUTTON';
-		document.getElementById('display').innerHTML = 'NEXT';
+		document.getElementById('display').innerHTML = ' ';
 		document.getElementById('host').innerHTML = "<img src=./images/hurry-aubrey.gif>";
 	} else {
 		startTime -= 1;
 		document.getElementById('display').innerHTML = 'You have ' + startTime + ' seconds left!';
+
 	}
 }
 
